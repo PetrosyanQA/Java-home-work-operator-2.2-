99bb99232e447987fa23d2c89645c4df4caf1dfa
Petrosyan Test <job-dream99@yandex.ru>
2022-11-19 14:30:48 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Итоговый счет");
-   int amount = 1000;
-   int addingAmount = 1100;
-   int balance = amount + addingAmount;
-   int bonus;
+        int amount = 1000;
+        int addingAmount = 1100;
+        int balance = amount + addingAmount;
+        int bonus;
 
-   if (addingAmount >= - 1000) {
-       bonus = (addingAmount/100);
-   } else { bonus = (addingAmount*1);}
+        if (addingAmount >= -1000) {
+            bonus = (addingAmount / 100);
+        } else {
+            bonus = (addingAmount * 1);
+        }
 
-   System.out.println(balance);
+        System.out.println(balance);
         System.out.println("Итоговый бонус");
         System.out.println(bonus);
 
 
-
     }
-
-
-
-
-
-
-
 
 
 }
