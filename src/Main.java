d22c9084793294c656561fe4de2a78d19382fb4b
Petrosyan Test <job-dream99@yandex.ru>
2022-12-10 02:56:51 +0400

    add commit

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Итоговый счет");
         int availableAmount = 1000;
-        int addingAmount = 2000;
+        int addingAmount;
+        addingAmount = 2000;
         int balanceOfUser = availableAmount + addingAmount;
         int numberOfBonus = 0;
 
 
         if (addingAmount > 1000) {
             numberOfBonus = addingAmount / 100;
-            if (addingAmount < 1000) {
-                numberOfBonus = 0;
-            }
         }
+
         System.out.println(balanceOfUser);
         System.out.println("Итоговый бонус");
         System.out.println(numberOfBonus);
-
-
     }
 
 
